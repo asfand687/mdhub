9338de4c4187ecc51702253b7bcaf3b1bf798881
asfand687 <asfandyar687@gmail.com>
2023-01-25 13:08:17 +0500

    added constants for nursing services

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const nursingAndHomecareServices = [
+  "Vital Signs Monitoring", "Post Operative Care", "Oncology Care", "Urology Care", "IV Therapy", "Foot Care", "Injections & Infusions", "Vaccination", "In Home/Outdoors Assistance", "Blood Work & Testing", "Medication Administration/ Monitoring", "Companion Care", "Elderly Care", "Transportation Assistance"
+]
