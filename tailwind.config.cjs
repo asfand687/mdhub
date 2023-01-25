f4bcff2367b7700fc5159e515ebfd9e7a397d579
asfand687 <asfandyar687@gmail.com>
2023-01-26 01:45:48 +0500

    fixed the type in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
         white: "-1px 1px 10px 1px rgba(255,255,255,0.90)",
         dark: "-1px 1px 20px 2px rgba(130,133,134,0.75)",
         cardService: "0px 6px 4px rgba(72, 171, 200, 0.4)",
-        input: "0px 6px 4px rgba(72, 171, 200, 0.4)";
+        input: "0px 6px 4px rgba(72, 171, 200, 0.4)",
       }
     },
   },
