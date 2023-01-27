df31a34a77e82617c8f9864029c8690c96d1c586
asfand687 <asfandyar687@gmail.com>
2023-01-27 19:15:23 +0500

    added video segment to how it works section

--- src/sections/Home/HomeHowItWorksSection.jsx
+++ src/sections/Home/HomeHowItWorksSection.jsx
@@ -6,10 +6,15 @@ import Slider from '../../components/Slider'
 
 const HomeHowItWorksSection = () => {
   return (
-    <section className="w-full max-w-[90rem] mx-auto py-10">
+    <section className="bg-how-it-works">
+      <article className="w-full max-w-[90rem] mx-auto pt-10 pb-20">
       <h2 className="text-4xl font-main text-center">How It Works</h2>
       <img className="w-44 mx-auto" src={HowItWorks01} />
       <Slider />
+        <div className="mx-auto w-full max-w-[1000px] h-[450px] mt-8 bg-gray-500 text-white flex justify-center items-center">
+          Video
+        </div>
+      </article>
     </section>
   )
 }
