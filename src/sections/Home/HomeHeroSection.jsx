e7a93dcc6f74ce66d894895afeb1234a772597fe
asfand687 <asfandyar687@gmail.com>
2023-02-10 23:37:58 +0500

    changed hero section

--- src/sections/Home/HomeHeroSection.jsx
+++ src/sections/Home/HomeHeroSection.jsx
@@ -15,12 +15,12 @@ const HomeHeroSection = () => {
           <p className="font-body text-center md:text-left font-extralight text-gray-900 md:text-xl py-4 md:py-8 max-w-[630px]">
             From meeting a doctor to ordering a perscription get the 360 healthcare you need all in one place.
           </p>
-          <div className='flex justify-center md:justify-start space-x-6 font-main text-gray-700'>
+          <div className='flex justify-center md:justify-start space-x-2 md:space-x-6 font-main text-gray-700'>
             <Button wide>
-              <span>See A Doctor</span> <FiArrowUpRight className="text-lg transition-colors ease-out duration-150 group-hover:text-primary" />
+              <span>See A Doctor</span> <FiArrowUpRight className="md:text-lg transition-colors ease-out duration-150 group-hover:text-primary" />
             </Button>
             <Button>
-              <span>Sign Up</span> <FiArrowUpRight className="text-lg transition-colors ease-out duration-150 group-hover:text-primary" />
+              <span>Sign Up</span> <FiArrowUpRight className="md:text-lg transition-colors ease-out duration-150 group-hover:text-primary" />
             </Button>
           </div>
         </article>
@@ -28,7 +28,7 @@ const HomeHeroSection = () => {
           <img className="w-full max-w-[550px]" src={Hero} alt="Hero" />
         </article>
       </section>
-      <h2 className="text-primary text-center font-main text-7xl py-10">
+      <h2 className="text-primary text-center font-main text-5xl md:text-7xl py-10">
         Get care today for <span className="text-gray-800">birth control.</span>
       </h2>
     </div>
