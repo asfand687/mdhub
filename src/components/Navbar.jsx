c78ad720902a3a76e4e87494d617d47d5838e202
asfand687 <asfandyar687@gmail.com>
2023-02-10 23:37:03 +0500

    added new buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom'
 import Logo from '../assets/logo.png'
 import { RxDotFilled } from 'react-icons/rx'
 import Drawer from './Drawer'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const [showDrawer, setShowDrawer] = useState(false)
   const location = useLocation()
+  const navigate = useNavigate()
+  const token = localStorage.getItem("jwtToken")
+
+  const handleSignout = () => {
+    localStorage.removeItem("jwtToken")
+    navigate("/")
+  }
 
   return (
     <nav className="md:border-b border-primary pb-[2px] md:pb-[6px] shadow">
@@ -18,7 +25,7 @@ const Navbar = () => {
             <img className="cursor-pointer w-28" src={Logo} alt="MD Hub" />
           </Link>
         </article>
-        <article className="hidden md:flex justify-between text-xl font-medium flex-1 font-main">
+        <article className="hidden lg:flex justify-between text-xl font-medium flex-1 font-main">
           <ul className="flex flex-1 justify-center items-center space-x-14">
             <li className="relative cursor-pointer hover:opacity-90 group">
             {
@@ -53,16 +60,33 @@ const Navbar = () => {
             <Link to="/services">services</Link>
           </li>
           </ul>
-          <div className="lg:pr-24 text-light text-base space-x-4 font-main font-light">
-            <button className="rounded-full px-9 py-1 border border-primary text-primary bg-transparent hover:bg-primary hover:text-white transition-all ease-in-out duration-300">
-              Login
-            </button>
-            <button className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
-              Sign Up
-            </button>
-          </div>
+          {
+            token ? (
+              <div className="lg:pr-24">
+                <button
+                onClick={handleSignout}
+                  className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
+                  Signout                 
+                </button>
+              </div>
+            ):
+            (
+              <div className="lg:pr-24 text-light text-base space-x-4 font-main font-light">
+                <Link to="/login">
+                  <button className="rounded-full px-9 py-1 border border-primary text-primary bg-transparent hover:bg-primary hover:text-white transition-all ease-in-out duration-300">
+                    Login
+                  </button>
+                </Link>
+                <Link to="/register">
+                  <button className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
+                    Sign Up
+                  </button>
+                </Link>
+              </div>
+            )
+          }
         </article>
-        <article onClick={() => setShowDrawer(!showDrawer)} className="absolute cursor-pointer flex flex-col justify-center space-y-1 top-0 right-0 bg-primary text-white py-6 px-4 md:py-8 md:px-6">
+        <article onClick={() => setShowDrawer(!showDrawer)} className="absolute cursor-pointer flex flex-col justify-center space-y-1 top-0 right-0 bg-primary text-white py-6 px-4 md:py-[1.7rem] lg:py-8 md:px-6">
           <span className="w-6 h-[2px] bg-white"></span>
           <span className="w-4 h-[2px] bg-white"></span>
         </article>
