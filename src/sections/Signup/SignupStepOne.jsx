a6c603535a585ed80c50b104ba710ba866a33df4
asfand687 <asfandyar687@gmail.com>
2023-02-13 23:29:02 +0500

    finished step one

--- src/sections/Signup/SignupStepOne.jsx
+++ src/sections/Signup/SignupStepOne.jsx
@@ -13,11 +13,24 @@ const SignupStepOne = () => {
   const showThirdFamilyMemberForm = registerationStore(state => state.showThirdFamilyMemberForm)
   const showFourthFamilyMemberForm = registerationStore(state => state.showFourthFamilyMemberForm)
 
-
-
-
   return (
     <section className='space-y-6'>
+      <article className="w-full self-start pt-8 pb-4 mb-4 ">
+        <div className="w-full flex justify-between items-center">
+          <article className="border-b-primary flex items-baseline gap-x-6 border-b-[3px] max-w-xs">
+            <h2 className="text-5xl ">Step 1</h2>
+            <div className="font-body text-xl font-light">
+              <div className="text-right">
+                <p>Enter member details</p>
+                <p className="text-sm">
+                  {selectedAccountType === "family" && "Upto 4 Family Members"}
+                  {selectedAccountType === "corporate" && "Upto 10 Employees"}
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </article>
       <div className="py-16 border space-y-4 px-8 shadow-cardService rounded-[35px] text-primary relative">
         <div className="w-full flex flex-wrap gap-y-4 gap-x-6">
           <input
