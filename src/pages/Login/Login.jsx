ebde05ddbafe789514cf4041ce485face06cb47c
asfand687 <asfandyar687@gmail.com>
2023-02-13 23:27:19 +0500

    made changes to login request

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,80 +1,71 @@
-import React, { useState } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import FormField from '../../components/FormField'
+import { registerationStore } from '../../store/registerationStore'
 
 const Login = () => {
   const navigate = useNavigate()
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-  })
-
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value })
-  }
+  const loginFormRef = useRef(null)
+  const setUserInfo = registerationStore(state => state.setUserInfo)
 
   const handleLogin = async (e) => {
     e.preventDefault()
+    const form = new FormData(e.target)
+    const loginFormData = Object.fromEntries(form)
     try {
-      const response = await fetch('http://localhost:8080/api/v1/auth/login', {
+      const response = await fetch('https://mdhub-backend.onrender.com/api/v1/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(form)
+        body: JSON.stringify(loginFormData)
       })
- 
-      setForm({
-        email: '',
-        password: ''
-      })
-
-      const data = await response.json()
-      localStorage.setItem("jwtToken", data.accessToken)
-      navigate('/dashboard')
-    } 
+      loginFormRef.current.reset()
+      if (response.ok) {
+        const data = await response.json()
+        localStorage.setItem("jwtToken", data.accessToken)
+        setUserInfo(data)
+        navigate('/dashboard')
+      }
+    }
     catch (error) {
-      console.log(error) 
+      console.log(error)
     }
   }
 
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken")
+    if (token) navigate("/dashboard")
+  }, [])
   return (
-    <section className="py-20 flex justify-center text-white">
-      <article className="flex items-center">
-        <div className="bg-primary rounded-tl-[35px] rounded-bl-[35px] p-10 font-main space-y-4 h-[300px] shadow-cardService">
-          <h1 className="text-4xl">Login</h1>
-        </div>
-        <div className="py-16 px-12 border bg-[#f9f9f9] shadow-cardService rounded-[35px] text-gray-800 font-body">
-          <h3 className="pb-4">Required Field *</h3>
-          <form onSubmit={handleLogin} className="space-y-4 w-[500px] text-gray-800">
-            <div className="w-full relative">
-              <p className="absolute left-0 -top-3">*</p>
-              <FormField
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  handleChange={handleChange}
-                  value={form.email}
-                />  
-            </div>
-            <div className="w-full relative">
-              <p className="absolute left-0 -top-3">*</p>
-              <FormField
-                type="password"
-                name="password"
-                placeholder="Password"
-                handleChange={handleChange}
-                value={form.password}
-              /> 
-            </div>
+    <section className="w-full max-w-[42rem] flex flex-col items-center mx-auto py-8 md:py-20 font-main">
+      <h2 className="text-4xl text-center pb-4">Login</h2>
+      <div className="py-16 border space-y-4 px-8 shadow-cardService rounded-[35px] text-primary relative">
+        <form ref={loginFormRef} onSubmit={handleLogin} className="space-y-4 w-[500px] text-gray-800">
+          <div className="w-full flex">
+            <input
+              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+              name="email"
+              type="email"
+              placeholder="Email*"
+              required
+            />
+          </div>
+          <div className="w-full flex">
+            <input
+              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+              name="password"
+              type="password"
+              placeholder="Password*"
+              required
+            />
+          </div>
             <div className='w-full pt-3'>
               <button type="submit" className={`rounded-full font-main text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
                 Login
               </button>
             </div>
           </form>
-        </div>
-      </article>
+      </div>
     </section>
   )
 }
